7fd971f7c1610e2d0d0e6d009facccce6ed3c1be
badunoff <badunoff@yahoo.com>
2020-09-18 21:48:28 +0200

    pandas and curl

--- app.py
+++ app.py
@@ -2,6 +2,29 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 
+import pandas as pd
+import json
+import os
+import pprint
+
+from urllib.request import urlopen
+
+import matplotlib.pyplot as plt
+
+import plotly.express as px
+
+import dash
+import dash_html_components as html
+
+
+df = pd.read_exel('agg.xlsx')
+with urlopen(
+    "https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json"
+) as response:
+    counties = json.load(response)
+
+
+
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
