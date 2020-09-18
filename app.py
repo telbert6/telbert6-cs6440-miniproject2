2f49b8e7121aa82472bed4bb91c03842b9a31928
badunoff <badunoff@yahoo.com>
2020-09-19 01:30:23 +0200

    map attempt

--- app.py
+++ app.py
@@ -78,6 +78,54 @@ app.layout = html.Div(
 )
 
 
+def get_options(list_stocks):
+    dict_list = []
+    for i in list_stocks:
+        dict_list.append({"label": i, "value": i})
+
+    return dict_list
+
+
+app.layout = html.Div(
+    [
+        html.Div(
+            className="row",
+            children=[
+                html.Div(
+                    className="four columns div-user-controls",
+                    children=[
+                        html.H2("DASH - STOCK PRICES"),
+                        html.P("Visualising time series with Plotly - Dash."),
+                        html.P("Pick one or more stocks from the dropdown below."),
+                        html.Div(
+                            className="div-for-dropdown",
+                            children=[
+                                dcc.Dropdown(
+                                    id="raceselector",
+                                    options=get_options(df["Race"].unique()),
+                                    multi=True,
+                                    value=[df["Race"].sort_values()[0]],
+                                    style={"backgroundColor": "#1E1E1E"},
+                                    className="raceselector",
+                                ),
+                            ],
+                            style={"color": "#1E1E1E"},
+                        ),
+                    ],
+                )
+            ],
+        ),
+        html.Div(
+            className="eight columns div-for-charts bg-grey",
+            children=[
+                dcc.Graph(id="usmap", config={"displayModeBar": False}, animate=True)
+            ],
+        ),
+    ]
+    # dcc.Markdown(children=top_markdown_text),
+)
+
+
 # Callback for timeseries price
 @app.callback(Output("usmap", "figure"), [Input("raceselector", "value")])
 def update_race(selected_dropdown_value):
@@ -93,55 +141,20 @@ def update_race(selected_dropdown_value):
 
     plot_df["% of Patients"] = plot_df.Cases / plot_df.Patients
 
-    trace1.append(
-        go.Choroplethmapbox(
-            geojson=counties,
-            locations=plot_df["fipscd"],
-            z=plot_df["% of Patients"],
-            colorscale="Viridis",
-            zmin=0,
-            zmax=12,
-            marker_opacity=0.5,
-            marker_line_width=0,
-        )
-    )
-
-    fig = go.Figure(
-        go.Choroplethmapbox(
-            geojson=counties,
-            locations=plot_df["fipscd"],
-            z=plot_df["% of Patients"],
-            colorscale="Viridis",
-            zmin=0,
-            zmax=12,
-            marker_opacity=0.5,
-            marker_line_width=0,
-        )
-    )
-    fig.update_layout(
+    fig = px.choropleth_mapbox(
+        plot_df,
+        geojson=counties,
+        locations="fipscd",
+        color="% of Patients",
+        color_continuous_scale="Viridis",
+        range_color=(0, plot_df["% of Patients"].max()),
         mapbox_style="carto-positron",
-        mapbox_zoom=3,
-        mapbox_center={"lat": 37.0902, "lon": -95.7129},
+        zoom=3,
+        center={"lat": 37.0902, "lon": -95.7129},
+        opacity=0.5,
+        labels={"% of Patients": "% of Patients"},
     )
     fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
-    
-
-    traces = [trace1]
-    data = [val for sublist in traces for val in sublist]
-    figure = {
-        "data": data,
-        "layout": go.Layout(
-            colorway=["#5E0DAC", "#FF4F00", "#375CB1", "#FF7400", "#FFF400", "#FF0056"],
-            template="plotly_dark",
-            paper_bgcolor="rgba(0, 0, 0, 0)",
-            plot_bgcolor="rgba(0, 0, 0, 0)",
-            margin={"b": 15},
-            hovermode="x",
-            autosize=True,
-            title={"text": "Stock Prices", "font": {"color": "white"}, "x": 0.5}#,
-            #xaxis={"range": [df_sub.index.min(), df_sub.index.max()]},
-        ),
-    }
 
     return fig
 
