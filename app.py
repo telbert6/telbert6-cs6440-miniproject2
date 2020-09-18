dbcce7de935ec17ed4990f748c16f862819e463e
badunoff <badunoff@yahoo.com>
2020-09-19 00:03:55 +0200

    map attempt

--- app.py
+++ app.py
@@ -63,10 +63,61 @@ app.layout = html.Div(
                     ],
                 )
             ],
-        )
+        ),
+		html.Div(className='eight columns div-for-charts bg-grey',
+				 children=[
+					 dcc.Graph(id='usmap', config={'displayModeBar': False}, animate=True)
+				 ])
     ]
     # dcc.Markdown(children=top_markdown_text),
 )
+
+
+
+# Callback for timeseries price
+@app.callback(Output('usmap', 'figure'),
+              [Input('raceselector', 'value')])
+def update_race(selected_dropdown_value):
+    trace1 = []
+
+	plot_df = pd.DataFrame(
+		conditions_by_county[conditions_by_county.Field == field]
+		.groupby("fipscd")
+		.agg({"Cases": sum, "Patients": sum})
+	).reset_index()
+
+	
+	
+    for stock in selected_dropdown_value:
+        trace1.append(go.Choroplethmapbox(
+			geojson=counties,
+			locations=plot_df['fipscd'],
+			z=plot_df["% of Patients"],
+			colorscale="Viridis",
+			zmin=0,
+			zmax=12,
+			marker_opacity=0.5,
+			marker_line_width=0,
+		))
+    traces = [trace1]
+    data = [val for sublist in traces for val in sublist]
+    figure = {'data': data,
+              'layout': go.Layout(
+                  colorway=["#5E0DAC", '#FF4F00', '#375CB1', '#FF7400', '#FFF400', '#FF0056'],
+                  template='plotly_dark',
+                  paper_bgcolor='rgba(0, 0, 0, 0)',
+                  plot_bgcolor='rgba(0, 0, 0, 0)',
+                  margin={'b': 15},
+                  hovermode='x',
+                  autosize=True,
+                  title={'text': 'Stock Prices', 'font': {'color': 'white'}, 'x': 0.5},
+                  xaxis={'range': [df_sub.index.min(), df_sub.index.max()]},
+              ),
+
+              }
+
+    return figure
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
