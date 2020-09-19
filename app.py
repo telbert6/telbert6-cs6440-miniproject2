9cc1162eeb207b4825bbeeca54ffc41204f176dd
badunoff <badunoff@yahoo.com>
2020-09-19 15:05:56 +0200

    presentable dash with 3 plots and 3 filters

--- app.py
+++ app.py
@@ -30,11 +30,42 @@ top_markdown_text = """
 This is my first deployed app
 """
 
+def state_center_zoom(state, df):
+	state_code = list(df[df.State == state]['fipscd'])[0][:2]
+	#print(f"code: {state_code}", file=sys.stderr)
+	
+	coords = list(pd.DataFrame([
+		list(pd.DataFrame(county['geometry']['coordinates'][0]).mean(axis = 0))
+		for county in counties['features']
+		if county['properties']['STATE'] == state_code
+	]).mean(axis = 0))
+	
+	print(f"coords: {coords}", file=sys.stderr)
+	
+	return coords, 4
 
 def get_options(list_stocks):
 	dict_list = []
 	for i in list_stocks:
 		dict_list.append({"label": i, "value": i})
+
+	return dict_list
+
+def get_options(list_stocks, add_all = False, add_all_label = "All"):
+	
+	if add_all:
+		dict_list= [{
+			"label":add_all_label,
+			"value":"All"
+		}]
+	else:
+		dict_list = []
+	
+	
+	for i in list_stocks:
+		dict_list.append({"label": i, "value": i})
+
+	
 
 	return dict_list
 
@@ -44,72 +75,48 @@ app.layout = html.Div(
 		html.Div(
 			className="row",
 			children=[
+				html.H2("Chronic Conditions Among Patients in 2008-2009"),
 				html.Div(
-					className="four columns div-user-controls",
+					className="twelve columns div-user-controls",
 					children=[
-						html.H2("DASH - STOCK PRICES"),
-						html.P("Visualising time series with Plotly - Dash."),
-						html.P("Pick one or more stocks from the dropdown below."),
 						html.Div(
 							className="div-for-dropdown",
 							children=[
-								dcc.Dropdown(
-									id="raceselector",
-									options=get_options(df["Race"].unique()),
-									multi=True,
-									value=[df["Race"].sort_values()[0]],
-									style={"backgroundColor": "#1E1E1E"},
-									className="raceselector",
-								),
-							],
-							style={"color": "#1E1E1E"},
-						),
-					],
-				)
-			],
-		),
-		html.Div(
-			className="eight columns div-for-charts bg-grey",
-			children=[
-				dcc.Graph(id="usmap", config={"displayModeBar": False}, animate=True)
-			],
-		),
-	]
-	# dcc.Markdown(children=top_markdown_text),
-)
-
-def get_options(list_stocks):
-	dict_list = []
-	for i in list_stocks:
-		dict_list.append({"label": i, "value": i})
-
-	return dict_list
-
-
-app.layout = html.Div(
-	[
-		html.Div(
-			className="row",
-			children=[
-				html.Div(
-					className="four columns div-user-controls",
-					children=[
-						html.H2("DASH - STOCK PRICES"),
-						html.P("Visualising time series with Plotly - Dash."),
-						html.P("Pick one or more stocks from the dropdown below."),
-						html.Div(
-							className="div-for-dropdown",
-							children=[
-								dcc.Dropdown(
-									id="raceselector",
-									options=get_options(df["Race"].unique()),
-									multi=True,
-									value=list(df["Race"].unique()),
-									style={"backgroundColor": "#1E1E1E"},
-									className="raceselector",
-								),
-							],
-							style={"color": "#1E1E1E"},
+								html.Div([
+									html.P('Race:'),
+									dcc.Dropdown(
+										id="raceselector",
+										options=get_options(df["Race"].unique()),
+										multi=True,
+										value=list(df["Race"].unique()),
+										style={"backgroundColor": "#1E1E1E"},
+									)
+								], className="three columns div-user-controls"),
+								html.Div([
+									html.P('Sex:'),
+									dcc.Dropdown(
+										id="sexselector",
+										options=get_options(
+											df["Sex"].unique(),
+											add_all = True, 
+											add_all_label = "Both"),
+										multi=False,
+										value="All"
+									)
+								], className="three columns div-user-controls"),
+								html.Div([
+									html.P('State:'),
+									dcc.Dropdown(
+										id="stateselector",
+										options=get_options(
+											df["State"].unique(),
+											add_all = True, 
+											add_all_label = "All"),
+										multi=False,
+										value="All"
+									)
+								], className="three columns div-user-controls")
+							]
 						),
 					],
 				)
@@ -120,71 +127,117 @@ app.layout = html.Div(
 				dcc.Graph(
 					id="usmap", 
 					config={"displayModeBar": False}
+				),
+				html.Div(
+					[
+						dcc.Graph(
+							id="race_pie", 
+							config={"displayModeBar": False}
+							,
+							className="four columns div-for-charts bg-grey"
+						),
+						dcc.Graph(
+							id="sex_pie", 
+							config={"displayModeBar": False},
+							className="four columns div-for-charts bg-grey"
+						)
+					]
 				)
 			],
-			className="eight columns div-for-charts bg-grey"
+			className="twelve columns div-for-charts bg-grey"
 		),
 	]
 	# dcc.Markdown(children=top_markdown_text),
 )
 
-
-
 # Callback for timeseries price
-@app.callback(Output("usmap", "figure"), [Input("raceselector", "value")])
-def update_race(selected_dropdown_value):
-	print(f"Running update on: {selected_dropdown_value}", file=sys.stderr)
+@app.callback(
+	
+	Output("usmap", "figure"), 
+	Output("race_pie", "figure"),
+	Output("sex_pie", "figure"),
+	[
+		Input("raceselector", "value"), 
+		Input("sexselector", "value"),
+		Input("stateselector", "value"),
+	]
+)
+def update_map(race_values, sex_value, state_value):
+	print(f"Running update on: {race_values}, {sex_value}, {state_value}", file=sys.stderr)
 
 	field = "SP_DEPRESSN"
 
+	all_race = "All" in race_values
+	all_sex = "All" == sex_value
+	all_states = "All" == state_value
+	
+	print(f"All filters: {all_race}, {all_sex}", file=sys.stderr)
+
 	trace1 = []
 
-	plot_df = pd.DataFrame(
-		df[
+
+	filtered_df = df[
 			(df.Field == field)
-			& df.Race.apply(lambda race: race in selected_dropdown_value)
+			& df.Race.apply(lambda race: all_race or race in race_values)
+			& df.Sex.apply(lambda sex: all_sex or sex == sex_value)
+			& df.State.apply(lambda state: all_states or state == state_value)
+			& (df.Patients >= 5)
 		]
+
+	map_plot_df = pd.DataFrame(
+		filtered_df
 		.groupby(["fipscd", "State", "County"])
 		.agg({"Cases": sum, "Patients": sum})
 	).reset_index()
-
+	map_plot_df["% of Patients"] = round(map_plot_df.Cases / map_plot_df.Patients * 100, 1)
+	print(map_plot_df.shape, file=sys.stderr)
 	
-
-
-	plot_df["% of Patients"] = plot_df.Cases / plot_df.Patients
-
-	print(plot_df, file=sys.stderr)
-
-
-	fig = px.choropleth_mapbox(
-		plot_df,
+	fig_map = px.choropleth_mapbox(
+		map_plot_df,
 		geojson=counties,
 		locations="fipscd",
 		color="% of Patients",
 		color_continuous_scale="Viridis",
-		range_color=(0, plot_df["% of Patients"].max()),
+		range_color=(0, map_plot_df["% of Patients"].max()),
 		mapbox_style="carto-positron",
 		zoom=3,
 		center={"lat": 37.0902, "lon": -95.7129},
 		opacity=0.5,
 		labels={"% of Patients": "% of Patients"},
-		hover_name = plot_df.apply(lambda x: f"{x['County']}, {x['State']}", axis = 1),
+		hover_name = map_plot_df.apply(lambda x: f"{x['County']}, {x['State']}", axis = 1),
 		hover_data = ['Cases', 'Patients']
 	)
-	fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
-
-	print(f"Update complete", file=sys.stderr)
+	fig_map.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
+	
+	#pie charts
+	race_pie_plot_df = pd.DataFrame(
+		filtered_df
+		.groupby(["Race"])
+		.agg({"Cases": sum, "Patients": sum})
+	).reset_index()
+	race_pie_fig = px.pie(
+		race_pie_plot_df, 
+		values='Cases', 
+		names='Race', 
+		title='Cases by Race'
+	)
 	
 	
-	return fig
+	sex_pie_plot_df = pd.DataFrame(
+		filtered_df
+		.groupby(["Sex"])
+		.agg({"Cases": sum, "Patients": sum})
+	).reset_index()
+	sex_pie_fig = px.pie(
+		sex_pie_plot_df, 
+		values='Cases', 
+		names='Sex', 
+		title='Cases by Sex'
+	)
+	
+	
+	return fig_map, race_pie_fig, sex_pie_fig
 
 
 if __name__ == '__main__':
 	app.run_server(debug=True)
-
-
-
-
-
-
-
