a5ede570ae877ddfdcda175aceafb83252ab5111
badunoff <badunoff@yahoo.com>
2020-09-19 11:12:56 +0200

    filter attempt

--- app.py
+++ app.py
@@ -141,6 +141,9 @@ def update_race(selected_dropdown_value):
         .groupby("fipscd")
         .agg({"Cases": sum, "Patients": sum})
     ).reset_index()
+	
+	print(plot_df.shape, file=sys.stderr)
+	print(plot_df, file=sys.stderr)
 
     plot_df["% of Patients"] = plot_df.Cases / plot_df.Patients
 
