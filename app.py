5188b311eb257a6c3bd8f2f84058cefceae27f75
badunoff <badunoff@yahoo.com>
2020-09-19 12:52:54 +0200

    filterable locally

--- app.py
+++ app.py
@@ -15,10 +15,10 @@ import matplotlib.pyplot as plt
 
 import plotly.express as px
 
-df = pd.read_csv("agg.csv")
+df = pd.read_csv("agg.csv", dtype={'fipscd': str})
 
 with open("counties.json") as json_file:
-    counties = json.load(json_file)
+	counties = json.load(json_file)
 
 external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
 
@@ -32,97 +32,100 @@ This is my first deployed app
 
 
 def get_options(list_stocks):
-    dict_list = []
-    for i in list_stocks:
-        dict_list.append({"label": i, "value": i})
+	dict_list = []
+	for i in list_stocks:
+		dict_list.append({"label": i, "value": i})
 
-    return dict_list
+	return dict_list
 
 
 app.layout = html.Div(
-    [
-        html.Div(
-            className="row",
-            children=[
-                html.Div(
-                    className="four columns div-user-controls",
-                    children=[
-                        html.H2("DASH - STOCK PRICES"),
-                        html.P("Visualising time series with Plotly - Dash."),
-                        html.P("Pick one or more stocks from the dropdown below."),
-                        html.Div(
-                            className="div-for-dropdown",
-                            children=[
-                                dcc.Dropdown(
-                                    id="raceselector",
-                                    options=get_options(df["Race"].unique()),
-                                    multi=True,
-                                    value=[df["Race"].sort_values()[0]],
-                                    style={"backgroundColor": "#1E1E1E"},
-                                    className="raceselector",
-                                ),
-                            ],
-                            style={"color": "#1E1E1E"},
-                        ),
-                    ],
-                )
-            ],
-        ),
-        html.Div(
-            className="eight columns div-for-charts bg-grey",
-            children=[
-                dcc.Graph(id="usmap", config={"displayModeBar": False}, animate=True)
-            ],
-        ),
-    ]
-    # dcc.Markdown(children=top_markdown_text),
+	[
+		html.Div(
+			className="row",
+			children=[
+				html.Div(
+					className="four columns div-user-controls",
+					children=[
+						html.H2("DASH - STOCK PRICES"),
+						html.P("Visualising time series with Plotly - Dash."),
+						html.P("Pick one or more stocks from the dropdown below."),
+						html.Div(
+							className="div-for-dropdown",
+							children=[
+								dcc.Dropdown(
+									id="raceselector",
+									options=get_options(df["Race"].unique()),
+									multi=True,
+									value=[df["Race"].sort_values()[0]],
+									style={"backgroundColor": "#1E1E1E"},
+									className="raceselector",
+								),
+							],
+							style={"color": "#1E1E1E"},
+						),
+					],
+				)
+			],
+		),
+		html.Div(
+			className="eight columns div-for-charts bg-grey",
+			children=[
+				dcc.Graph(id="usmap", config={"displayModeBar": False}, animate=True)
+			],
+		),
+	]
+	# dcc.Markdown(children=top_markdown_text),
 )
 
 def get_options(list_stocks):
-    dict_list = []
-    for i in list_stocks:
-        dict_list.append({"label": i, "value": i})
+	dict_list = []
+	for i in list_stocks:
+		dict_list.append({"label": i, "value": i})
 
-    return dict_list
+	return dict_list
 
 
 app.layout = html.Div(
-    [
-        html.Div(
-            className="row",
-            children=[
-                html.Div(
-                    className="four columns div-user-controls",
-                    children=[
-                        html.H2("DASH - STOCK PRICES"),
-                        html.P("Visualising time series with Plotly - Dash."),
-                        html.P("Pick one or more stocks from the dropdown below."),
-                        html.Div(
-                            className="div-for-dropdown",
-                            children=[
-                                dcc.Dropdown(
-                                    id="raceselector",
-                                    options=get_options(df["Race"].unique()),
-                                    multi=True,
-                                    value=list(df["Race"].unique()),
-                                    style={"backgroundColor": "#1E1E1E"},
-                                    className="raceselector",
-                                ),
-                            ],
-                            style={"color": "#1E1E1E"},
-                        ),
-                    ],
-                )
-            ],
-        ),
-        html.Div(
-            className="eight columns div-for-charts bg-grey",
-            children=[
-                dcc.Graph(id="usmap", config={"displayModeBar": False}, animate=True)
-            ],
-        ),
-    ]
-    # dcc.Markdown(children=top_markdown_text),
+	[
+		html.Div(
+			className="row",
+			children=[
+				html.Div(
+					className="four columns div-user-controls",
+					children=[
+						html.H2("DASH - STOCK PRICES"),
+						html.P("Visualising time series with Plotly - Dash."),
+						html.P("Pick one or more stocks from the dropdown below."),
+						html.Div(
+							className="div-for-dropdown",
+							children=[
+								dcc.Dropdown(
+									id="raceselector",
+									options=get_options(df["Race"].unique()),
+									multi=True,
+									value=list(df["Race"].unique()),
+									style={"backgroundColor": "#1E1E1E"},
+									className="raceselector",
+								),
+							],
+							style={"color": "#1E1E1E"},
+						),
+					],
+				)
+			],
+		),
+		html.Div(
+			[
+				dcc.Graph(
+					id="usmap", 
+					config={"displayModeBar": False}
+				)
+			],
+			className="eight columns div-for-charts bg-grey"
+		),
+	]
+	# dcc.Markdown(children=top_markdown_text),
 )
 
 
@@ -130,46 +133,54 @@ app.layout = html.Div(
 # Callback for timeseries price
 @app.callback(Output("usmap", "figure"), [Input("raceselector", "value")])
 def update_race(selected_dropdown_value):
-    print(f"Running update on: {selected_dropdown_value}", file=sys.stderr)
+	print(f"Running update on: {selected_dropdown_value}", file=sys.stderr)
 
-    field = "SP_DEPRESSN"
+	field = "SP_DEPRESSN"
 
-    trace1 = []
+	trace1 = []
 
-    plot_df = pd.DataFrame(
-        df[
-            (df.Field == field)
-            & df.Race.apply(lambda race: race in selected_dropdown_value)
-        ]
-        .groupby(["fipscd", "State", "County"])
-        .agg({"Cases": sum, "Patients": sum})
-    ).reset_index()
+	plot_df = pd.DataFrame(
+		df[
+			(df.Field == field)
+			& df.Race.apply(lambda race: race in selected_dropdown_value)
+		]
+		.groupby(["fipscd", "State", "County"])
+		.agg({"Cases": sum, "Patients": sum})
+	).reset_index()
 
-    print(plot_df.shape, file=sys.stderr)
-    print(plot_df, file=sys.stderr)
+	
 
-    plot_df["% of Patients"] = plot_df.Cases / plot_df.Patients
 
-    fig = px.choropleth_mapbox(
-        plot_df,
-        geojson=counties,
-        locations="fipscd",
-        color="% of Patients",
-        color_continuous_scale="Viridis",
-        range_color=(0, plot_df["% of Patients"].max()),
-        mapbox_style="carto-positron",
-        zoom=3,
-        center={"lat": 37.0902, "lon": -95.7129},
-        opacity=0.5,
-        labels={"% of Patients": "% of Patients"},
-    )
-    fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
+	plot_df["% of Patients"] = plot_df.Cases / plot_df.Patients
 
-    print(f"Update complete", file=sys.stderr)
+	print(plot_df, file=sys.stderr)
+
+
+	fig = px.choropleth_mapbox(
+		plot_df,
+		geojson=counties,
+		locations="fipscd",
+		color="% of Patients",
+		color_continuous_scale="Viridis",
+		range_color=(0, plot_df["% of Patients"].max()),
+		mapbox_style="carto-positron",
+		zoom=3,
+		center={"lat": 37.0902, "lon": -95.7129},
+		opacity=0.5,
+		labels={"% of Patients": "% of Patients"},
+		hover_name = plot_df.apply(lambda x: f"{x['County']}, {x['State']}", axis = 1),
+		hover_data = ['Cases', 'Patients']
+	)
+	fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
+
+	print(f"Update complete", file=sys.stderr)
+	
+	
+	return fig
 
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+	app.run_server(debug=True)
 
 
 
